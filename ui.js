6e750307be8fb851a868773ccc64d9be2d30adfd
Ahmed-shaker32 <141178764+Ahmed-shaker32@users.noreply.github.com>
2026-03-05 12:27:58 +0200

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,11 @@
+function renderSchedules(schedules) {
+  const container = document.getElementById("resultsContent");
 
+  container.innerHTML = "";
+
+  schedules.forEach(schedule => {
+    const div = document.createElement("div");
+    div.innerText = schedule.name;
+    container.appendChild(div);
+  });
+}
