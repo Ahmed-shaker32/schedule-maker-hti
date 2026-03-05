0f3402a3d0d40a674242c146d0bbcb7d33170d5c
Ahmed-shaker32 <141178764+Ahmed-shaker32@users.noreply.github.com>
2026-03-05 13:13:35 +0200

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,69 @@
+async function saveGroupsToDB(name, data) {
+
+  const user = await getUser();
+
+  await supabaseClient
+    .from("groups")
+    .insert([
+      {
+        name: name,
+        data: data,
+        user_id: user.id
+      }
+    ]);
+}
+
+async function loadGroups() {
+
+  const user = await getUser();
+
+  const { data } = await supabaseClient
+    .from("groups")
+    .select("*")
+    .eq("user_id", user.id);
+
+  return data;
+}
+
+async function deleteGroup(id) {
+
+  await supabaseClient
+    .from("groups")
+    .delete()
+    .eq("id", id);
+}
+
+async function saveSchedule(name, data) {
+
+  const user = await getUser();
+
+  await supabaseClient
+    .from("schedules")
+    .insert([
+      {
+        name: name,
+        data: data,
+        user_id: user.id
+      }
+    ]);
+}
+
+async function loadSchedules() {
+
+  const user = await getUser();
+
+  const { data } = await supabaseClient
+    .from("schedules")
+    .select("*")
+    .eq("user_id", user.id);
+
+  return data;
+}
+
+async function deleteSchedule(id) {
+
+  await supabaseClient
+    .from("schedules")
+    .delete()
+    .eq("id", id);
+}
